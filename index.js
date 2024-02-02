0f341e6008cf56a87bfcc83e2e33f0075425305c
khushbu.rani <khushbu.rani@sworks.co.in-at-919795083283>
2024-02-02 14:53:52 +0530

    changes in ile

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("khu rani");
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+console.log(typeof NaN);  //number
+
+console.log(typeof { a: 1 }) //object
+console.log(typeof [1, 2, 4])  // object
+
+let add = function (a, b) {   // anonymous function is function which has no name
+    return a + b;
+}
+let result = add(5, 3);
+console.log(result)
