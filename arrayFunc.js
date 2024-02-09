4e62dcc205c87b4c7d68d3d19229a6909c8fecb8
khushbu.rani <khushbu.rani@sworks.co.in-at-919795083283>
2024-02-09 15:10:42 +0530

    array methods commited

---
+++ arrayFunc.js
@@ -0,0 +1,53 @@
+const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+const result = words.filter((word) => word.length > 6);
+console.log(result);
+
+const evenNumber = [3, 4, 5, 6, 8, 10, 12, 13, 18];
+const resEvenNumber = evenNumber.filter((n) => {
+    return n % 2 == 0
+})
+console.log(resEvenNumber);
+
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const resData = data.filter((res) => {
+    return res > 4
+})
+
+let people = [
+    { name: 'Rachel', age: 24 },
+    { name: 'David', age: 16 },
+    { name: 'Aakash', age: 23 },
+    { name: 'Franklin', age: 26 },
+    { name: 'Ricky', age: 17 }
+];
+
+let adults = people.filter(res => {
+    return res.age > 18
+})
+let adultsAletrnateOption = people.filter(big => big.age >= 18);
+console.log(adults);
+
+
+let evenNumberMap = [3, 4, 5, 6, 8, 10, 12, 13, 18];
+evenNumberMap = evenNumberMap.map((n) => {
+    if (n % 2 == 0) { return `Odd = ${n}` } else { return `Even = ${n}` }
+});
+console.log(evenNumberMap);
+
+let evenNumberMapCopy = [3, 4, 5, 6, 8, 10, 12, 13, 18];
+evenNumberMapCopy.map((n) => {
+    return n * 2
+});
+console.log(evenNumberMapCopy);
+
+
+let evenNumberMapCopyFor = [3, 4, 5, 6, 8, 10, 12, 13, 18];
+let dataTest = [];
+evenNumberMapCopyFor.forEach((n) => {
+    dataTest.push(n * 2)
+});
+console.log(dataTest);
+
+
+
+// Note: We need to learn reduce method in this File.
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 console.log(typeof null);  // object
 console.log(typeof undefined);  // undefined
 console.log(typeof NaN);  //number
-
 console.log(typeof { a: 1 }) //object
 console.log(typeof [1, 2, 4])  // object
 
@@ -9,4 +8,26 @@ let add = function (a, b) {   // anonymous function is function which has no nam
     return a + b;
 }
 let result = add(5, 3);
-console.log(result)
+console.log(result);
+
+//  find method returns the first array element that passes a certain test function.
+//  find method doesnot change the original array and 
+// The method find() returns undefined if none of the elements in the array passes the test.
+
+let number = [12, 16, 18, 20, 22];
+let data = number.find(element => element > 20);
+let data1 = number.find(element => element >= 20);
+let data2 = number.find(element => element >= 25);
+console.log(data);
+console.log(data1);
+console.log(data2);
+console.log(number);
+
+// findIndex returns the first array element index that passes a certain test function.
+//The method findIndex() returns -1 if none of the elements in the array passes the test.
+const array1 = [5, 12, 8, 130, 44];
+const isLargeNumber = (element) => element > 13;
+const isLargeNumber1 = (element => element > 140);
+console.log(array1.findIndex(isLargeNumber));
+console.log(array1.findIndex(isLargeNumber1));
+console.log(array1)
